cd185b83301635edf918d548e3f77b89fa018368
IgorpetinDev <igorpetindev@gmail.com>
2026-04-25 17:20:45 +0300

    a50: add device specific makefile
    Change-Id: I7006bb93fece7bd5580086479044f295796e227a
    Signed-off-by: IgorpetinDev <igorpetindev@gmail.com>

---
+++ device.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 IgorpetinDev
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 28
+
+# Disable dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := false
+
