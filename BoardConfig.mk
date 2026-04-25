f980129369924f4d06fdefaf61f8028a7916bed3
IgorpetinDev <igorpetindev@gmail.com>
2026-04-25 17:22:06 +0300

    a50: create a board config makefile
    Change-Id: I80cc74253a1842bbeff351bc7bb3be85cd0cee14
    Signed-off-by: IgorpetinDev <igorpetindev@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 IgorpetinDev
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Device path
+DEVICE_PATH := device/samsung/a50
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
