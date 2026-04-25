2c9c05c0f9b53084483d659da5bdae8ba4d6be24
IgorpetinDev <igorpetindev@gmail.com>
2026-04-25 18:02:05 +0300

    a50: Fix health-hal-2-1
    Change-Id: I1529e6656dcb2e784b323afb081120c626e88851
    Signed-off-by: IgorpetinDev <igorpetindev@gmail.com>

--- device.mk
+++ device.mk
@@ -15,3 +15,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := false
 PRODUCT_PACKAGES += \
 	android.hardware.fastboot@1.0-impl-mock \
 	fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+	android.hardware.health@2.1-impl \
+	android.hardware.health@2.1-impl.recovery \
+	android.hardware.health@2.1-service
